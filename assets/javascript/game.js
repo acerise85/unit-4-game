bef43f90f6d37fbc4f6d36718f983909b6dc07a6
acerise <42525915+acerise85@users.noreply.github.com>
2018-11-20 16:40:40 -0700

    Random numbers working for images and winning number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,37 @@
 //Variables
-var crystalPics = 'assets/images/';
+var crystalPics = 'assets/images/crystal' + '.jpg';
 var win = 0;
 var lose = 0;
 var counter = 0;
 var minNumber = 19;
 var maxNumber = 120;
 var guessMinnumber = 1;
-var guessMaxnumber = 0;
+var guessMaxnumber = 12;
 
 var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber +1) + minNumber);
 var winningNumber = 0;
 $('#winningNumber').text(randomNumber);
 document.getElementById('winningNumber').innerHTML = randomNumber;
 
-$('#crystalPics').each(function(){
-    var crystalNumbers = Math.floor(Math.random() * (guessMaxnumber - guessMinnumber) + guessMinnumber);
-    crystalPics = $(this);
+var crystalValue = [0, 0, 0, 0];
 
-    crystalPics.attr('assets/images/crystal', 'crystal' + crystalNumbers + '.jpg');
-    crystalPics.attr('alt', 'assets/images/crystal: ' + crystalPics.attr('assets/images/crystal'));
-    console.log(crystalNumbers);
-})
+var crystalNumbers = Math.floor(Math.random() * (guessMaxnumber - guessMinnumber) + guessMinnumber);
+
+
+
+for(var i = 0; i < crystalValue.length; i++){
+
+    crystalValue.length = 3;
+    
+    crystalValue.push(crystalNumbers);
+        console.log(crystalNumbers);
+    var crystalPics = $("<img>");
+
+    
+
+}
+
+   
 
 
 
